38461ad136012bcdbc88ca10d9ae1033cd1d6c19
Rishi Karri <rishikarri1@gmail.com>
2025-01-12 18:47:21 -0500

    feat: client side render

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
+import S3KeyCount from '../pages/s3KeyCount'
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        Number of S3 Keys in file: 
+        <S3KeyCount />
       </main>
       
     </div>
---
+++ fe/src/pages/s3KeyCount.tsx
@@ -0,0 +1,33 @@
+"use client"; // This is a client component 👈🏽
+
+
+import axios from 'axios'; // Import axios for making HTTP requests
+import { useState, useEffect } from 'react';
+
+export default function S3KeyCount() {
+  const [s3KeysCount, setS3KeysCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://b7xj1i3tki.execute-api.us-east-1.amazonaws.com/s3-keys-length2'); // Replace with your actual API endpoint
+
+        console.log('response', response)
+        setS3KeysCount(2); 
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        Number of S3 Keys in file: 3
+      </main>
+      
+    </div>
+  );
+}
