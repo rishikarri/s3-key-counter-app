24b7ae4ed804cd6bc96356387c0d4e63c842ffea
Rishi Karri <rishikarri1@gmail.com>
2025-01-12 23:21:14 -0500

    feat: add css

--- fe/src/app/page.tsx
+++ fe/src/app/page.tsx
@@ -4,19 +4,19 @@ import S3KeyCount from '../pages/s3KeyCount';
 import DataPoster from '../pages/dataPoster'; 
 import { useState } from 'react';
 import { Tabs, TabList, TabPanel, Tab } from 'react-tabs'; 
-import 'react-tabs/style/react-tabs.css';
+import 'react-tabs/style/react-tabs.css'; // Re-import react-tabs styles
 
 export default function Home() {
   const [tabIndex, setTabIndex] = useState(0);
 
   return (
-    <div className="container mx-auto p-4">  {/* Base container styles */}
-      <h1 className="text-2xl font-bold mb-4">S3 Counter App</h1>
-      <div className="bg-gray-100 p-6 rounded-lg shadow-md"> {/* Tab container styles */}
+    <div className="container mx-auto p-4"> 
+      <h1 className="text-2xl font-bold mb-4">S3 Key Counter</h1>
+      <div className="bg-gray-100 p-6 rounded-lg shadow-md"> 
         <Tabs selectedIndex={tabIndex} onSelect={index => setTabIndex(index)}>
-          <TabList className="flex mb-4"> {/* Tab list styles */}
-            <Tab className="mr-4 px-4 py-2 rounded-md bg-blue-500 text-white">S3 Key Count</Tab>
-            <Tab className="px-4 py-2 rounded-md bg-gray-200">Data Poster</Tab>
+          <TabList className="flex mb-4"> 
+            <Tab className="mr-4 px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300">S3 Key Count</Tab> 
+            <Tab className="px-4 py-2 rounded-md bg-gray-200 hover:bg-gray-300">Data Poster</Tab> 
           </TabList>
 
           <TabPanel>
--- fe/src/pages/dataPoster.tsx
+++ fe/src/pages/dataPoster.tsx
@@ -82,7 +82,7 @@ const DataPoster: React.FC = () => {
 				<button
 					type="button"
 					onClick={handleAddInput}
-					className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+					className="mt-4 mr-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
 				>
 					Add Key-Value Pair
 				</button>
