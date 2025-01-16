1ba1cbea486c8d4b5ba3a2e54c8dc13b2b70f2a9
Rishi Karri <rishikarri1@gmail.com>
2025-01-16 18:22:43 -0500

    feat: update s3writer

--- be/iac/s3Writer/index.js
+++ be/iac/s3Writer/index.js
@@ -1,31 +1,32 @@
-const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({}); // Use default AWS credentials and region
 
+
 exports.handler = async (event) => {
   try {
+    console.log('event', event);
+
+    const reqBodyData = JSON.parse(event.body) // TODO: add validation to make sure incoming event is valid JSON
+    const data = reqBodyData.payload
+
     const params = {
       Bucket: process.env.BUCKET_NAME, // Replace with your S3 bucket name
-      Key: 'data.json' // Replace with the desired file name in S3
+      Key: 'data.json', // Replace with the desired file name in S3
+      Body: JSON.stringify(data) 
     };
 
-    const command = new GetObjectCommand(params);
+    const command = new PutObjectCommand(params);
     const response = await s3Client.send(command);
 
-    const data = JSON.parse(await response.Body.transformToString()); // Read and parse the object from S3
-
-    const numberOfKeys = Object.keys(data).length; 
-
+    console.log('Successfully uploaded data to S3:', response); 
     return {
       statusCode: 200,
-      body: JSON.stringify({ numKeys: numberOfKeys })
+      body: JSON.stringify({ message: 'Data has been uploaded to S3 successfully', event })
     };
 
   } catch (err) {
-    console.error('Error reading from S3:', err);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Error reading from S3', error: err.message })
-    };
+    console.error('Error uploading to S3:', err);
+    throw err; 
   }
 };
